28ef390d24ab8e29ea9dbbe491eb917b88b1b789
Mihai Potra <mike@mpotra.com>
2016-05-14 19:59:41 +0300

    Expose WritableStream and TransferStream to tests

--- run-whatwg-tests.js
+++ run-whatwg-tests.js
@@ -2,12 +2,14 @@ const glob = require('glob');
 const path = require('path');
 
 
-import ReadableStreamWHATWG from './whatwg/lib/readable-stream';
+//import ReadableStream from './whatwg/lib/readable-stream';
 import ReadableStream from './lib/ReadableStream';
-import WritableStream from './whatwg/lib/writable-stream';
+//import WritableStream from './whatwg/lib/writable-stream';
+import WritableStream from './lib/WritableStream';
 import ByteLengthQueuingStrategy from './whatwg/lib/byte-length-queuing-strategy';
 import CountQueuingStrategy from './whatwg/lib/count-queuing-strategy';
-import TransformStream from './whatwg/lib/transform-stream';
+//import TransformStream from './whatwg/lib/transform-stream';
+import TransformStream from './lib/TransformStream';
 
 global.ReadableStream = ReadableStream;
 global.WritableStream = WritableStream;
@@ -21,7 +23,7 @@ if (process.argv.length === 2) {
   //const tests = glob.sync(__path('readable-byte-stream.js'));
   //const tests = glob.sync(__path('*.internal.js'));
   const tests = glob.sync(__path('*.js'));
-  
+  //console.log('tests order:\r\n', tests.join('\r\n'),'\r\n');
   tests.forEach(require);
 } else {
   glob.sync(path.resolve(process.argv[2])).forEach(require);
